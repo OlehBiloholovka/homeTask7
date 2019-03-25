97f8adb9c79e7565e7c762ae6523aeed59c6f276
OlehBiloholovka <35770198+OlehBiloholovka@users.noreply.github.com>
2019-03-26 00:48:52 +0200

    added angular variable

--- src/app/components/username/username.component.ts
+++ src/app/components/username/username.component.ts
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./username.component.css']
 })
 export class UsernameComponent implements OnInit {
+  username: string;
 
   constructor() { }
 
   ngOnInit() {
+    this.username = '';
   }
 
 }
